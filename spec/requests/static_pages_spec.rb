dfebdc2e66ca81739a9d89db33be709ee0be04b0
Geoff Petrie <g.petrie@gmail.com>
2013-06-24 16:25:55 -0600

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,17 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact")
+    end
+  end
+
 end
